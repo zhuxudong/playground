a306c10745cb3d45ec75859df5f7423ada693c21
GuoLei1990 <gl3336563@163.com>
2021-03-02 18:34:20 +0800

    refactor: adpter to latest engine

--- demos/buffer-geometry/index.ts
+++ demos/buffer-geometry/index.ts
@@ -3,35 +3,35 @@ import {
   BlinnPhongMaterial,
   Buffer,
   BufferBindFlag,
-  BufferGeometry,
   BufferUsage,
   Camera,
   Color,
   DirectLight,
   Engine,
-  GeometryRenderer,
   IndexFormat,
+  Mesh,
+  MeshRenderer,
   SystemInfo,
   Vector3,
-  Vector4,
   VertexElement,
   VertexElementFormat,
   WebGLEngine
 } from "oasis-engine";
 
 /**
- * to create custom cube geometry.
+ * To create custom cube geometry.
  */
 class CustomCubeGeometry {
   /**
-   * create cube geometry with custom BufferGeometry.
-   * @param size - cube size
+   * Create cube geometry with custom BufferGeometry.
+   * @param size - Cube size
+   * @returns Cube mesh
    */
-  static create(engine: Engine, size: number): BufferGeometry {
-    const geometry = new BufferGeometry(engine, "CustomCubeGeometry");
+  static create(engine: Engine, size: number): Mesh {
+    const geometry = new Mesh(engine, "CustomCubeGeometry");
 
     // prettier-ignore
-    // create vertices data.
+    // Create vertices data.
     const vertices: Float32Array = new Float32Array([
     	// up
     	-size, size, -size, 0, 1, 0, size, size, -size, 0, 1, 0, size, size, size, 0, 1, 0, -size, size, size, 0, 1, 0,
@@ -47,7 +47,7 @@ class CustomCubeGeometry {
     	-size, size, -size, 0, 0, -1, size, size, -size, 0, 0, -1, size, -size, -size, 0, 0, -1, -size, -size, -size, 0, 0, -1]);
 
     // prettier-ignore
-    // create indices data.
+    // Create indices data.
     const indices: Uint16Array = new Uint16Array([
     	// up
     	0, 2, 1, 2, 0, 3,
@@ -62,56 +62,55 @@ class CustomCubeGeometry {
     	// back
     	20, 22, 23, 22, 20, 21]);
 
-    // create gpu vertex buffer and index buffer.
+    // Create gpu vertex buffer and index buffer.
     const vertexBuffer = new Buffer(engine, BufferBindFlag.VertexBuffer, vertices, BufferUsage.Static);
     const indexBuffer = new Buffer(engine, BufferBindFlag.IndexBuffer, indices, BufferUsage.Static);
 
-    // bind buffer
+    // Bind buffer
     geometry.setVertexBufferBinding(vertexBuffer, 24);
     geometry.setIndexBufferBinding(indexBuffer, IndexFormat.UInt16);
 
-    // add vertexElement
+    // Add vertexElement
     geometry.setVertexElements([
       new VertexElement("POSITION", 0, VertexElementFormat.Vector3, 0),
       new VertexElement("NORMAL", 12, VertexElementFormat.Vector3, 0)
     ]);
 
-    // add one sub geometry.
-    geometry.addSubGeometry(0, indices.length);
+    // Add one sub geometry.
+    geometry.addSubMesh(0, indices.length);
     return geometry;
   }
 }
 
-// create engine and get root entity.
+// Create engine and get root entity.
 const engine = new WebGLEngine("o3-demo");
 const canvas = engine.canvas;
 const rootEntity = engine.sceneManager.activeScene.createRootEntity("Root");
 canvas.width = window.innerWidth * SystemInfo.devicePixelRatio;
 canvas.height = window.innerHeight * SystemInfo.devicePixelRatio;
 
-// create light.
+// Create light.
 const lightEntity = rootEntity.createChild("DirectLight");
 const ambient = lightEntity.addComponent(AmbientLight);
 const directLight = lightEntity.addComponent(DirectLight);
 ambient.color = new Color(0.2, 0.2, 0.2);
 directLight.color = new Color(0.3, 0.4, 0.4);
 
-// create camera.
+// Create camera.
 const cameraEntity = rootEntity.createChild("Camera");
 cameraEntity.transform.setPosition(0, 6, 10);
 cameraEntity.transform.lookAt(new Vector3(0, 0, 0));
 cameraEntity.addComponent(Camera);
 
-// create custom cube.
-// use CustomCubeGeometry.create() to create cube geometry.
+// Create custom cube.
+// Use CustomCubeGeometry.create() to create cube geometry.
 const cubeEntity = rootEntity.createChild("Cube");
-const cubeRenderer = cubeEntity.addComponent(GeometryRenderer);
+const cubeRenderer = cubeEntity.addComponent(MeshRenderer);
 const cubeGeometry = CustomCubeGeometry.create(engine, 1.0);
 const material = new BlinnPhongMaterial(engine);
-cubeEntity.transform.rotateXYZ(0, 60, 0);
-material.ambientColor = new Color(1, 1, 1, 1);
-cubeRenderer.geometry = cubeGeometry;
-cubeRenderer.material = material;
+cubeEntity.transform.rotate(0, 60, 0);
+cubeRenderer.mesh = cubeGeometry;
+cubeRenderer.setMaterial(material);
 
-// run engine.
+// Run engine.
 engine.run();
