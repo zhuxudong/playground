46dab0485cfb0d0100aadc233bbad9517818403a
GuoLei <gl3336563@163.com>
2021-04-02 16:13:04 +0800

    Refactor: opt mesh demos (#20)
    * feat: add transform demo
    
    * refctaor: update buffer mesh demo
    
    * fix: material texture display error bug.
    
    * refactor: opt mesh demo

--- demos/primitive-mesh/index.ts
+++ demos/primitive-mesh/index.ts
@@ -16,73 +16,86 @@ import {
   WebGLEngine
 } from "oasis-engine";
 
-// Create engine
-const engine = new WebGLEngine("o3-demo");
-engine.canvas.width = window.innerWidth * SystemInfo.devicePixelRatio;
-engine.canvas.height = window.innerHeight * SystemInfo.devicePixelRatio;
+init();
 
-// Create root entity
-const rootEntity = engine.sceneManager.activeScene.createRootEntity();
+function init(): void {
+  // Create engine
+  const engine = new WebGLEngine("o3-demo");
+  engine.canvas.width = window.innerWidth * SystemInfo.devicePixelRatio;
+  engine.canvas.height = window.innerHeight * SystemInfo.devicePixelRatio;
 
-// Create camera
-const cameraEntity = rootEntity.createChild("Camera");
-cameraEntity.transform.setPosition(0, 0, 20);
-cameraEntity.addComponent(Camera);
-cameraEntity.addComponent(OrbitControl);
+  // Create root entity
+  const rootEntity = engine.sceneManager.activeScene.createRootEntity();
 
-// Create direct light
-const lightEntity = rootEntity.createChild("DirectLight");
-const light = lightEntity.addComponent(DirectLight);
-light.intensity = 0.3;
+  // Create camera
+  const cameraEntity = rootEntity.createChild("Camera");
+  cameraEntity.transform.setPosition(0, 0, 20);
+  cameraEntity.addComponent(Camera);
+  cameraEntity.addComponent(OrbitControl);
 
-engine.resourceManager
-  .load<Texture2D>({
-    url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*ArCHTbfVPXUAAAAAAAAAAAAAARQnAQ",
-    type: AssetType.Texture2D
-  })
-  .then((texture) => {
-    const distanceX = 2.5;
-    const distanceY = 2.5;
-    const position = new Vector3();
+  // Create direct light
+  const lightEntity = rootEntity.createChild("DirectLight");
+  const light = lightEntity.addComponent(DirectLight);
+  light.intensity = 0.3;
 
-    // Create material
-    const material = new BlinnPhongMaterial(engine);
-    material.emissiveTexture = texture;
-    material.emissiveColor.setValue(1, 1, 1, 1);
+  engine.resourceManager
+    .load<Texture2D>({
+      url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*ArCHTbfVPXUAAAAAAAAAAAAAARQnAQ",
+      type: AssetType.Texture2D
+    })
+    .then((texture) => {
+      const distanceX = 2.5;
+      const distanceY = 2.5;
+      const position = new Vector3();
 
-    for (let i = 0; i < 3; i++) {
-      const posX = (i - 1) * distanceX;
+      // Create material
+      const material = new BlinnPhongMaterial(engine);
+      material.emissiveTexture = texture;
+      material.emissiveColor.setValue(1, 1, 1, 1);
 
-      // Create cuboid
-      position.setValue(posX, distanceY * 2.5, 0);
-      generatePrimitiveEntity("cuboid", position, material, PrimitiveMesh.createCuboid(engine));
+      for (let i = 0; i < 3; i++) {
+        const posX = (i - 1) * distanceX;
 
-      // Create sphere
-      position.setValue(posX, distanceY * 1.5, 0);
-      generatePrimitiveEntity("sphere", position, material, PrimitiveMesh.createSphere(engine));
+        // Create cuboid
+        position.setValue(posX, distanceY * 2.5, 0);
+        generatePrimitiveEntity(rootEntity, "cuboid", position, material, PrimitiveMesh.createCuboid(engine));
 
-      // Create plane
-      position.setValue(posX, distanceY * 0.5, 0);
-      generatePrimitiveEntity("plane", position, material, PrimitiveMesh.createPlane(engine));
+        // Create sphere
+        position.setValue(posX, distanceY * 1.5, 0);
+        generatePrimitiveEntity(rootEntity, "sphere", position, material, PrimitiveMesh.createSphere(engine));
 
-      // Create cylinder
-      position.setValue(posX, -distanceY * 0.5, 0);
-      generatePrimitiveEntity("cylinder", position, material, PrimitiveMesh.createCylinder(engine));
+        // Create plane
+        position.setValue(posX, distanceY * 0.5, 0);
+        generatePrimitiveEntity(rootEntity, "plane", position, material, PrimitiveMesh.createPlane(engine));
 
-      // Create cone
-      position.setValue(posX, -distanceY * 1.5, 0);
-      generatePrimitiveEntity("cone", position, material, PrimitiveMesh.createCone(engine));
+        // Create cylinder
+        position.setValue(posX, -distanceY * 0.5, 0);
+        generatePrimitiveEntity(rootEntity, "cylinder", position, material, PrimitiveMesh.createCylinder(engine));
 
-      // Create turos
-      position.setValue(posX, -distanceY * 2.5, 0);
-      generatePrimitiveEntity("torus", position, material, PrimitiveMesh.createTorus(engine));
-    }
-  });
+        // Create cone
+        position.setValue(posX, -distanceY * 1.5, 0);
+        generatePrimitiveEntity(rootEntity, "cone", position, material, PrimitiveMesh.createCone(engine));
 
-// Run engine
-engine.run();
+        // Create turos
+        position.setValue(posX, -distanceY * 2.5, 0);
+        generatePrimitiveEntity(rootEntity, "torus", position, material, PrimitiveMesh.createTorus(engine));
+      }
+    });
 
-function generatePrimitiveEntity(name: string, position: Vector3, material: Material, mesh: ModelMesh): Entity {
+  // Run engine
+  engine.run();
+}
+
+/**
+ * generate primitive mesh entity.
+ */
+function generatePrimitiveEntity(
+  rootEntity: Entity,
+  name: string,
+  position: Vector3,
+  material: Material,
+  mesh: ModelMesh
+): Entity {
   const entity = rootEntity.createChild(name);
   entity.transform.setPosition(position.x, position.y, position.z);
   entity.addComponent(RotateScript);
@@ -93,8 +106,16 @@ function generatePrimitiveEntity(name: string, position: Vector3, material: Mate
   return entity;
 }
 
+/**
+ * Script for rotate.
+ */
 class RotateScript extends Script {
-  onUpdate() {
+  /**
+   * @override
+   * The main loop, called frame by frame.
+   * @param deltaTime - The deltaTime when the script update.
+   */
+  onUpdate(deltaTime: number): void {
     this.entity.transform.rotate(0.5, 0.6, 0);
   }
 }
