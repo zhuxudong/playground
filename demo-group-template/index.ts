91375e74d3233b85f18d9fba59a3e084ea41c8bf
zhuxudong <callzhuxudong@163.com>
2021-04-13 16:36:31 +0800

    ci: support secondary directory with vite and demosify (#48)

---
+++ demo-group-template/index.ts
@@ -0,0 +1,18 @@
+import { OrbitControl } from "@oasis-engine/controls";
+import { Camera, SystemInfo, Vector3, WebGLEngine } from "oasis-engine";
+
+// Create engine object
+const engine = new WebGLEngine("o3-demo");
+engine.canvas.width = window.innerWidth * SystemInfo.devicePixelRatio;
+engine.canvas.height = window.innerHeight * SystemInfo.devicePixelRatio;
+
+const scene = engine.sceneManager.activeScene;
+const rootEntity = scene.createRootEntity();
+
+// Create camera
+const cameraEntity = rootEntity.createChild("Camera");
+cameraEntity.transform.position = new Vector3(0, 0, 50);
+cameraEntity.addComponent(Camera);
+cameraEntity.addComponent(OrbitControl);
+
+engine.run();
