9451a001847907b27e5fc13c6461675a7a6c9428
zhuxudong <callzhuxudong@163.com>
2021-05-20 13:54:14 +0800

    feat: add light-probe-global demo

---
+++ demos/advance/light-probe-global/index.ts
@@ -0,0 +1,99 @@
+import { SphericalHarmonics3Baker } from "@oasis-engine/baker";
+import { OrbitControl } from "@oasis-engine/controls";
+import * as dat from "dat.gui";
+import {
+  AssetType,
+  BackgroundMode,
+  Camera,
+  Color,
+  DiffuseMode,
+  DirectLight,
+  Logger,
+  MeshRenderer,
+  PBRMaterial,
+  PrimitiveMesh,
+  SkyBoxMaterial,
+  SphericalHarmonics3,
+  TextureCubeMap,
+  Vector3,
+  WebGLEngine
+} from "oasis-engine";
+
+Logger.enable();
+// create engine object
+let engine = new WebGLEngine("o3-demo");
+engine.canvas.resizeByClientSize();
+
+let scene = engine.sceneManager.activeScene;
+const rootEntity = scene.createRootEntity();
+const gui = new dat.GUI();
+
+// env light debug
+let envFolder = gui.addFolder("Env Light");
+envFolder.add(scene.ambientLight, "specularIntensity", 0, 1);
+envFolder.add(scene.ambientLight, "diffuseIntensity", 0, 1);
+envFolder.open();
+
+// Direct light
+let directLightNode = rootEntity.createChild("dir_light");
+let directLight = directLightNode.addComponent(DirectLight);
+directLightNode.transform.rotate(-30, 0, 0);
+directLight.color = new Color(1, 1, 1);
+directLight.intensity = 0.2;
+
+// Create camera
+let cameraNode = rootEntity.createChild("camera_node");
+cameraNode.transform.position = new Vector3(-3, 0, 3);
+cameraNode.addComponent(Camera);
+cameraNode.addComponent(OrbitControl);
+
+// Create sky
+const sky = scene.background.sky;
+const skyMaterial = new SkyBoxMaterial(engine);
+scene.background.mode = BackgroundMode.Sky;
+sky.material = skyMaterial;
+sky.mesh = PrimitiveMesh.createCuboid(engine, 1, 1, 1);
+
+// material ball
+const ball = rootEntity.createChild("ball");
+const ballRender = ball.addComponent(MeshRenderer);
+const material = new PBRMaterial(engine);
+material.metallicFactor = 0;
+material.roughnessFactor = 0;
+ballRender.mesh = PrimitiveMesh.createSphere(engine, 1, 64);
+ballRender.setMaterial(material);
+
+engine.resourceManager
+  .load<TextureCubeMap>({
+    urls: [
+      "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*TUkMQpLvsGYAAAAAAAAAAAAAARQnAQ",
+      "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*1PF-Q5j3HKYAAAAAAAAAAAAAARQnAQ",
+      "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*cY8-QLCjqrIAAAAAAAAAAAAAARQnAQ",
+      "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*LTrfR619LjIAAAAAAAAAAAAAARQnAQ",
+      "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*qrYcQYE-SOoAAAAAAAAAAAAAARQnAQ",
+      "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*o_QqQI9ii9wAAAAAAAAAAAAAARQnAQ"
+    ],
+    type: AssetType.TextureCube
+  })
+  .then((cubeMap) => {
+    skyMaterial.textureCubeMap = cubeMap;
+    scene.ambientLight.specularTexture = cubeMap;
+
+    const sh = new SphericalHarmonics3();
+    SphericalHarmonics3Baker.fromTextureCubeMap(cubeMap, sh);
+    scene.ambientLight.diffuseSphericalHarmonics = sh;
+    scene.ambientLight.diffuseMode = DiffuseMode.SphericalHarmonics;
+
+    gui
+      .add({ bake: true }, "bake")
+      .name("烘焙")
+      .onChange((v) => {
+        if (v) {
+          scene.ambientLight.diffuseMode = DiffuseMode.SphericalHarmonics;
+        } else {
+          scene.ambientLight.diffuseMode = DiffuseMode.SolidColor;
+        }
+      });
+  });
+
+engine.run();
