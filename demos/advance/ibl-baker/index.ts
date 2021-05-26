a8c849486f99a2744dd7faad1eb049b7f44ce435
zhuxudong <callzhuxudong@163.com>
2021-05-26 16:22:32 +0800

    refactor: use engine baker

--- demos/advance/ibl-baker/index.ts
+++ demos/advance/ibl-baker/index.ts
@@ -1,4 +1,4 @@
-import { EncodingMode, SphericalHarmonics3Baker } from "@oasis-engine/baker";
+import { EncodingMode, SphericalHarmonics3Baker, IBLBaker, IBLBakerConfig } from "@oasis-engine/baker";
 import { OrbitControl } from "@oasis-engine/controls";
 import * as dat from "dat.gui";
 import {
@@ -28,8 +28,6 @@ import {
   WebGLEngine,
   WebGLMode
 } from "oasis-engine";
-import vertex from "./vertex.js";
-import frag from "./frag.js";
 Logger.enable();
 
 const gui = new dat.GUI();
@@ -57,89 +55,35 @@ sky.mesh = PrimitiveMesh.createCuboid(engine, 1, 1, 1);
 // Create box
 const boxEntity = rootEntity.createChild("box");
 const sphereMaterial = new PBRMaterial(engine);
-sphereMaterial.roughnessFactor = 0;
 const renderer = boxEntity.addComponent(MeshRenderer);
 renderer.mesh = PrimitiveMesh.createSphere(engine, 1, 64);
 renderer.setMaterial(sphereMaterial);
 
 engine.resourceManager
   .load<TextureCubeMap>({
-    // url: "https://gw.alipayobjects.com/os/bmw-prod/10c5d68d-8580-4bd9-8795-6f1035782b94.bin", // sunset_1K
+    url: "https://gw.alipayobjects.com/os/bmw-prod/10c5d68d-8580-4bd9-8795-6f1035782b94.bin", // sunset_1K
     // url: "https://gw.alipayobjects.com/os/bmw-prod/20d58ffa-c7da-4c54-8980-4efaf91a0239.bin",// pisa_1K
-    url: "https://gw.alipayobjects.com/os/bmw-prod/59b28d9f-7589-4d47-86b0-52c50b973b10.bin", // footPrint_2K
+    // url: "https://gw.alipayobjects.com/os/bmw-prod/59b28d9f-7589-4d47-86b0-52c50b973b10.bin", // footPrint_2K
     type: AssetType.HDR
   })
   .then((cubeMap) => {
-    ambientLight.specularTexture = cubeMap;
-    ambientLight.specularMode = SpecularMode.HDR;
+    const bakedTexture = IBLBaker.fromTextureCubeMap(cubeMap, EncodingMode.RGBE, {
+      sampleCount: 1024
+    }) as any;
 
-    skyMaterial.textureCubeMap = cubeMap;
-    testIBL(cubeMap);
+    ambientLight.specularMode = SpecularMode.HDR;
+    skyMaterial.textureCubeMap = bakedTexture;
+    ambientLight.specularTexture = bakedTexture;
+    // ambientLight.specularTexture = cubeMap;
 
     // const sh = new SphericalHarmonics3();
     // SphericalHarmonics3Baker.fromTextureCubeMap(cubeMap, sh, EncodingMode.RGBE);
     // ambientLight.diffuseMode = DiffuseMode.SphericalHarmonics;
     // ambientLight.diffuseSphericalHarmonics = sh;
+
+    //debug
+    gui.add(sphereMaterial, "metallicFactor", 0, 1, 0.1);
+    gui.add(sphereMaterial, "roughnessFactor", 0, 10, 1);
+
+    engine.run();
   });
-
-function testIBL(texture: TextureCubeMap) {
-  engine.pause();
-
-  // Test full-screen
-  const iblShader = Shader.create("Oasis-IBL-baker", vertex, frag);
-  const boxEntity = rootEntity.createChild("box");
-  const material = new Material(engine, iblShader);
-  const renderer = boxEntity.addComponent(MeshRenderer);
-  renderer.mesh = PrimitiveMesh.createPlane(engine, 2, 2);
-  renderer.setMaterial(material);
-
-  const maxMipLevels = texture.mipmapCount;
-
-  material.shaderData.setTexture("environmentMap", texture);
-  material.shaderData.setVector2("textureInfo", new Vector2(256, maxMipLevels));
-
-  const renderTarget = new RenderTarget(
-    engine,
-    256,
-    256,
-    new RenderColorTexture(engine, 256, 256, undefined, true, true),
-    RenderBufferDepthFormat.Depth
-  );
-  boxEntity.layer = Layer.Layer10;
-  camera.cullingMask = Layer.Layer10;
-  camera.renderTarget = renderTarget;
-
-  for (let face = 0; face < 6; face++) {
-    for (let lod = 0; lod < maxMipLevels; lod++) {
-      material.shaderData.setFloat("face", face);
-      //   const roughness = lod / (maxMipLevels - 1);
-      let alpha = Math.pow(2, lod) / 256;
-      if (lod === 0) {
-        alpha = 0;
-      }
-      material.shaderData.setFloat("linearRoughness", alpha);
-
-      camera.render(TextureCubeFace.PositiveX + face, lod);
-    }
-  }
-
-  //   material.shaderData.setFloat("face", 0);
-  //   material.shaderData.setFloat("linearRoughness", 0);
-
-  // revert
-  camera.cullingMask = Layer.Everything;
-  camera.renderTarget = null;
-  boxEntity.destroy();
-
-  const renderColorTexture: any = renderTarget.getColorTexture();
-  renderColorTexture.filterMode = TextureFilterMode.Trilinear;
-  ambientLight.specularTexture = renderColorTexture;
-  renderTarget.destroy();
-
-  skyMaterial.textureCubeMap = renderColorTexture;
-  engine.resume();
-
-  //debug
-  gui.add(sphereMaterial, "metallicFactor", 0, 1, 0.1);
-  gui.add(sphereMaterial, "roughnessFactor", 0, 10, 0.01);
-}
