2e1cb924ae955b326d36d2e8e67a7bfe26024fe8
zhuxudong <callzhuxudong@163.com>
2021-05-26 23:55:54 +0800

    feat: add ibl debug

--- demos/advance/ibl-baker/index.ts
+++ demos/advance/ibl-baker/index.ts
@@ -1,11 +1,13 @@
-import { EncodingMode, SphericalHarmonics3Baker, IBLBaker, IBLBakerConfig } from "@oasis-engine/baker";
+import { EncodingMode, SphericalHarmonics3Baker, IBLBaker } from "@oasis-engine/baker";
 import { OrbitControl } from "@oasis-engine/controls";
 import * as dat from "dat.gui";
 import {
   AssetType,
   BackgroundMode,
   Camera,
+  CullMode,
   DiffuseMode,
+  Entity,
   Layer,
   Logger,
   Material,
@@ -19,6 +21,7 @@ import {
   SkyBoxMaterial,
   SpecularMode,
   SphericalHarmonics3,
+  Texture2D,
   TextureCubeFace,
   TextureCubeMap,
   TextureFilterMode,
@@ -32,7 +35,7 @@ Logger.enable();
 
 const gui = new dat.GUI();
 //-- create engine object
-let engine = new WebGLEngine("o3-demo", { alpha: false, webGLMode: WebGLMode.WebGL2 });
+let engine = new WebGLEngine("o3-demo", { alpha: false });
 engine.canvas.resizeByClientSize();
 
 let scene = engine.sceneManager.activeScene;
@@ -41,7 +44,7 @@ const rootEntity = scene.createRootEntity();
 
 //Create camera
 let cameraNode = rootEntity.createChild("camera_node");
-cameraNode.transform.position = new Vector3(0, 0, 5);
+cameraNode.transform.position = new Vector3(0, 0, 10);
 const camera = cameraNode.addComponent(Camera);
 cameraNode.addComponent(OrbitControl);
 
@@ -52,26 +55,19 @@ background.mode = BackgroundMode.Sky;
 sky.material = skyMaterial;
 sky.mesh = PrimitiveMesh.createCuboid(engine, 1, 1, 1);
 
-// Create box
-const boxEntity = rootEntity.createChild("box");
-const sphereMaterial = new PBRMaterial(engine);
-const renderer = boxEntity.addComponent(MeshRenderer);
-renderer.mesh = PrimitiveMesh.createSphere(engine, 1, 64);
-renderer.setMaterial(sphereMaterial);
-
 engine.resourceManager
   .load<TextureCubeMap>({
-    url: "https://gw.alipayobjects.com/os/bmw-prod/10c5d68d-8580-4bd9-8795-6f1035782b94.bin", // sunset_1K
+    // url: "https://gw.alipayobjects.com/os/bmw-prod/10c5d68d-8580-4bd9-8795-6f1035782b94.bin", // sunset_1K
     // url: "https://gw.alipayobjects.com/os/bmw-prod/20d58ffa-c7da-4c54-8980-4efaf91a0239.bin",// pisa_1K
-    // url: "https://gw.alipayobjects.com/os/bmw-prod/59b28d9f-7589-4d47-86b0-52c50b973b10.bin", // footPrint_2K
+    url: "https://gw.alipayobjects.com/os/bmw-prod/59b28d9f-7589-4d47-86b0-52c50b973b10.bin", // footPrint_2K
     type: AssetType.HDR
   })
   .then((cubeMap) => {
-    const bakedTexture = IBLBaker.fromTextureCubeMap(cubeMap, true) as any;
+    cubeMap = IBLBaker.fromTextureCubeMap(cubeMap, true) as any;
 
     ambientLight.specularMode = SpecularMode.HDR;
-    // skyMaterial.textureCubeMap = bakedTexture;
-    ambientLight.specularTexture = bakedTexture;
+    // skyMaterial.textureCubeMap = cubeMap;
+    ambientLight.specularTexture = cubeMap;
 
     // ambientLight.specularTexture = cubeMap;
 
@@ -80,9 +76,105 @@ engine.resourceManager
     // ambientLight.diffuseMode = DiffuseMode.SphericalHarmonics;
     // ambientLight.diffuseSphericalHarmonics = sh;
 
-    //debug
-    gui.add(sphereMaterial, "metallicFactor", 0, 1, 0.1);
-    gui.add(sphereMaterial, "roughnessFactor", 0, 10, 1);
-
     engine.run();
+
+    debugIBL(cubeMap);
   });
+
+function debugIBL(texture: TextureCubeMap) {
+  Shader.create(
+    "ibl debug test",
+    `
+    attribute vec3 POSITION;
+    attribute vec2 TEXCOORD_0;
+
+    uniform mat4 u_MVPMat;
+    varying vec2 v_uv;
+
+    void main(){
+      gl_Position = u_MVPMat * vec4(POSITION, 1.0);
+      v_uv = TEXCOORD_0;
+  }
+  `,
+    `
+    uniform sampler2D u_env;
+    uniform int face;
+    varying vec2 v_uv;
+
+    vec4 RGBEToLinear(vec4 value) {
+      return vec4( step(0.0, value.a) * value.rgb * exp2( value.a * 255.0 - 128.0 ), 1.0 );
+    }
+  
+
+    void main(){
+      vec2 uv = v_uv;
+      if(face == 2){
+        uv.x = v_uv.y;
+        uv.y= 1.0 - v_uv.x;
+      }else if(face == 3){
+        uv.x = 1.0 - v_uv.y;
+        uv.y=  v_uv.x;
+      }
+      gl_FragColor = texture2D(u_env, uv);
+      gl_FragColor = RGBEToLinear(gl_FragColor);
+      gl_FragColor.rgb = pow(gl_FragColor.rgb, vec3(1.0 / 2.2));
+      gl_FragColor.a= 1.0;
+    }
+    `
+  );
+  const size = texture.width;
+
+  // Create Sphere
+  const sphereEntity = rootEntity.createChild("box");
+  sphereEntity.transform.setPosition(-1, 2, 0);
+  const sphereMaterial = new PBRMaterial(engine);
+  sphereMaterial.roughnessFactor = 0;
+  const renderer = sphereEntity.addComponent(MeshRenderer);
+  renderer.mesh = PrimitiveMesh.createSphere(engine, 1, 64);
+  renderer.setMaterial(sphereMaterial);
+
+  // Create planes
+  const planes = new Array<Entity>(6);
+  const planeMaterials = new Array<Material>(6);
+
+  for (let i = 0; i < 6; i++) {
+    const bakerEntity = rootEntity.createChild("IBL Baker Entity");
+    const bakerMaterial = new Material(engine, Shader.find("ibl debug test"));
+    bakerMaterial.renderState.rasterState.cullMode = CullMode.Off;
+    const bakerRenderer = bakerEntity.addComponent(MeshRenderer);
+    bakerRenderer.mesh = PrimitiveMesh.createPlane(engine, 2, 2);
+    bakerRenderer.setMaterial(bakerMaterial);
+    planes[i] = bakerEntity;
+    planeMaterials[i] = bakerMaterial;
+  }
+
+  planes[0].transform.setPosition(1, 0, 0); // PX
+  planes[1].transform.setPosition(-3, 0, 0); // NX
+  planes[2].transform.setPosition(1, 2, 0); // PY
+  planes[3].transform.setPosition(1, -2, 0); // NY
+  planes[4].transform.setPosition(-1, 0, 0); // PZ
+  planes[5].transform.setPosition(3, 0, 0); // NZ
+
+  //debug
+  gui.add(sphereMaterial, "metallicFactor", 0, 1, 0.01);
+  gui.add(sphereMaterial, "roughnessFactor", 0, 1, 0.01);
+
+  function changeMip(mipLevel: number) {
+    const mipSize = size >> mipLevel;
+    for (let i = 0; i < 6; i++) {
+      const material = planeMaterials[i];
+      const data = new Uint8Array(mipSize * mipSize * 4);
+      texture.getPixelBuffer(TextureCubeFace.PositiveX + i, 0, 0, mipSize, mipSize, data, mipLevel);
+      const planeTexture = new Texture2D(engine, mipSize, mipSize, undefined, false); // no mipmap
+      planeTexture.setPixelBuffer(data);
+      material.shaderData.setTexture("u_env", planeTexture);
+      material.shaderData.setInt("face", i);
+    }
+  }
+
+  changeMip(0);
+
+  gui.add({ mipLevel: 0 }, "mipLevel", 0, texture.mipmapCount - 1).onChange((mipLevel: number) => {
+    changeMip(mipLevel);
+  });
+}
